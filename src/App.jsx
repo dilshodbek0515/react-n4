1fa84b0da5a3e468c11537ed28418d40955431f6
dilshod <dilshodx516@gmail.com>
2024-10-29 15:29:48 +0500

    todolist

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 function App() {
-
+  const inc = () => {
+    toast("Hello world")
+  }
   return (
     <>
-      
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <button onClick={inc}>Toastify</button>
+      <ToastContainer />
     </>
   )
 }
